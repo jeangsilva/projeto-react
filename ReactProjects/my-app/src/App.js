16d0d291116e7ddfe271d12e305b1fd9bb41db60
Jean Silva <je.ang.10@hotmail.com>
2022-10-19 15:05:21 -0300

    atv

---
+++ ReactProjects/my-app/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Jean Gabriel Silva
+        </p>
+        <a
+          className="App-link"
+          href="https://softexrecife.org.br/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Softex - Goiana - PE
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
