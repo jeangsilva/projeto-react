82ab0fa1327fe857f6eea613065531c4f5d10104
Jean Silva <je.ang.10@hotmail.com>
2022-11-23 10:12:12 -0300

    projeto 2

--- ReactProjects/my-app/src/App.js
+++ ReactProjects/my-app/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import HelloWorld from './components/Helloworlds';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <p>
           Jean Gabriel Silva
         </p>
+        <HelloWorld/>
         <a
           className="App-link"
           href="https://softexrecife.org.br/"
